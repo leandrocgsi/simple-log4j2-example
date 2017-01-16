6278e2abb52100f1ad9e8a2343f25745c1cc738a
leandrocgsi <leandrocgsi@gmail.com>
2017-01-16 13:27:22 -0200

    Starting code

---
+++ src/main/java/br/com/erudio/HelloLog4j2.java
@@ -0,0 +1,15 @@
+package br.com.erudio;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class HelloLog4j2 {
+	
+    static final Logger logger = LogManager.getLogger(HelloLog4j2.class);
+
+    public boolean callMe() {
+        logger.entry();
+        logger.error("Inside Hello Logger!");
+        return logger.exit(false);
+    }
+}
