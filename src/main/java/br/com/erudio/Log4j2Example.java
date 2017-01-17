215f9dfbdd7a847876eaa1eb75f132d5bf933126
leandrocgsi <leandrocgsi@gmail.com>
2017-01-17 13:27:54 -0200

    Starting code

---
+++ src/main/java/br/com/erudio/Log4j2Example.java
@@ -0,0 +1,17 @@
+package br.com.erudio;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4j2Example {
+    
+    static final Logger logger = LogManager.getLogger(Log4j2Example.class);
+    
+    public static void main(String[] args) {
+        
+        logger.trace("Entering Log4j Example.");
+        HelloLog4j2 hello = new HelloLog4j2();
+        if (!hello.callMe()) logger.error("Ohh!Failed!");
+        logger.trace("Exiting Log4j Example.");
+    }
+}
